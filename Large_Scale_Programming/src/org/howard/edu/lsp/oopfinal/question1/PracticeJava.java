b1ec2650269878db26666134f188c36abc67bcb5
pkritika <kritika.pant@bison.howard.edu>
2024-05-01 16:03:06 -0400

    Practice for final exam

---
+++ Large_Scale_Programming/src/org/howard/edu/lsp/oopfinal/question1/PracticeJava.java
@@ -0,0 +1,8 @@
+package org.howard.edu.lsp.oopfinal.question1;
+
+public class PracticeJava {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		
+	}
+}
---
+++ Large_Scale_Programming/src/org/howard/edu/lsp/oopfinal/question3/PracticeJava.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.oopfinal.question3;
+
+public class PracticeJava {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		
+	}
+}
+
+
